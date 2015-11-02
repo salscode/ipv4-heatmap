1792c29b96b190100d505bc5639c21acc7474750
Sal Sodano <sal@skytoaster.com>
2015-11-02 18:42:03 -0500

    Change Location table to use Auto Increment key.

--- db/geodata.sql
+++ db/geodata.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `locations` (
-  `locid` mediumint(8) unsigned NOT NULL,
+  `locid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `latitude` float(8,4) NOT NULL,
   `longitude` float(8,4) NOT NULL,
   `ipcount` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`locid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=0;
